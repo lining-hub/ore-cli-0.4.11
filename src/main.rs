462272b625816a4c681558b2e3df0bf12d71e0a4
momodaka <463435681@qq.com>
2024-04-06 22:17:26 +0800

    fix: fix build

--- src/main.rs
+++ src/main.rs
@@ -197,7 +197,7 @@ async fn main() {
 
     // Initialize miner.
     let cluster = args.rpc.unwrap_or(cli_config.json_rpc_url);
-    let post_cluster = args.post_prc.unwrap_or(cluster);
+    let post_cluster = args.post_prc.unwrap_or(cluster.clone());
     let default_keypair = args.keypair.unwrap_or(cli_config.keypair_path);
 
     let miner = Arc::new(Miner::new(
